03eb70b35671ae5e6ee3d5536dd4770fbc72c9ac
hustlzp <hustlzp@qq.com>
2015-10-23 10:52:00 +0800

    Use Umeng as social share SDK.

--- xcz/Constants.h
+++ xcz/Constants.h
@@ -7,6 +7,10 @@
 #define RunOnSimulator 0
 #endif
 
+#define AVOSCloudAppID  @"9pq709je4y36ubi10xphdpovula77enqrz27idozgry7x644"
+#define AVOSCloudAppKey @"364g5iu6q1f3hhwz8wruvmayu3vsap995wh4ws7tszor43np"
+#define UmengAppKey @"5408794dfd98c533af00180a"
+
 #define IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
 #define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
 #define IS_RETINA ([[UIScreen mainScreen] scale] >= 2.0)
---
+++ xcz/XCZAppDelegate.m
@@ -0,0 +1,183 @@
+//
+//  XCZAppDelegate.m
+//  xcz
+//
+//  Created by 刘志鹏 on 14-6-28.
+//  Copyright (c) 2014年 Zhipeng Liu. All rights reserved.
+//
+
+#import "XCZWork.h"
+#import "XCZQuote.h"
+#import "XCZAppDelegate.h"
+#import "XCZOtherViewController.h"
+#import "XCZRandomQuoteViewController.h"
+#import "XCZLibraryViewController.h"
+#import "XCZWorkDetailViewController.h"
+#import "XCZMeetViewController.h"
+#import "XCZLikesViewController.h"
+#import "XCZUtils.h"
+#import "Constants.h"
+#import "UIColor+Helper.h"
+#import "DownloadFont.h"
+#import "Constants.h"
+#import "UMSocial.h"
+#import <AVOSCloud/AVOSCloud.h>
+#import <ionicons/IonIcons.h>
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
+#import <FMDB/FMDB.h>
+
+@implementation XCZAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [Fabric with:@[[Crashlytics class]]];
+
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    // AVOSCloud
+    [AVOSCloud setApplicationId:AVOSCloudAppID clientKey:AVOSCloudAppKey];
+    
+    // 友盟
+    [UMSocialData setAppKey:UmengAppKey];
+    
+    // 执行数据库拷贝
+    [self copyPublicDatabase];
+    [self copyUserDatabase];
+    
+    // 延迟0.5s
+    usleep(500 * 1000);
+    
+//    [DownloadFont asynchronouslyDownloadFont:@"STFangsong" completion:nil];
+    
+    // 摘录
+    XCZRandomQuoteViewController *quoteController = [XCZRandomQuoteViewController new];
+    UINavigationController *quoteNavController = [[UINavigationController alloc] initWithRootViewController:quoteController];
+    quoteController.tabBarItem.title = @"摘录";
+    UIImage *quotesImg = [UIImage imageNamed:@"quotes.png"];
+    quoteController.tabBarItem.image = quotesImg;
+    
+    // 偶遇
+    XCZMeetViewController *meetController = [XCZMeetViewController new];
+    UINavigationController *meetNavController = [[UINavigationController alloc] initWithRootViewController:meetController];
+    meetController.tabBarItem.title = @"偶遇";
+    UIImage *meetImg = [UIImage imageNamed:@"authors.png"];
+    meetController.tabBarItem.image = meetImg;
+    
+    // 文库
+    XCZLibraryViewController *libraryController = [XCZLibraryViewController new];
+    UINavigationController *libraryNavController = [[UINavigationController alloc] initWithRootViewController:libraryController];
+    libraryController.tabBarItem.title = @"文库";
+    UIImage *libraryImg = [UIImage imageNamed:@"works.png"];
+    libraryController.tabBarItem.image = libraryImg;
+    
+    // 其他
+    XCZOtherViewController *otherController = [[XCZOtherViewController alloc] init];
+    UINavigationController *meNavController = [[UINavigationController alloc] initWithRootViewController:otherController];
+    UIImage *infoIcon = [IonIcons imageWithIcon:ion_ios_more size:34 color:[UIColor XCZSystemGrayColor]];
+    UIImage *selectedInfoIcon = [IonIcons imageWithIcon:ion_ios_more size:34 color:[UIColor XCZSystemTintColor]];
+    otherController.tabBarItem.title = @"其他";
+    otherController.tabBarItem.image = infoIcon;
+    otherController.tabBarItem.selectedImage = selectedInfoIcon;
+    
+    // TabBar Controller
+    UITabBarController *tabBarController = [[UITabBarController alloc] init];
+    tabBarController.viewControllers = @[quoteNavController, meetNavController, libraryNavController, meNavController];
+    
+    // Root Controller
+    [self.window setRootViewController:tabBarController];
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+}
+
+- (void)copyUserDatabase
+{
+    NSString *storePath = [XCZUtils getUserDatabaseFilePath];
+    NSString *bundleStore = [[NSBundle mainBundle] pathForResource:@"xcz_user" ofType:@"db"];
+    
+    // 若Documents文件夹下不存在数据库文件，则执行拷贝
+    if (![[NSFileManager defaultManager] fileExistsAtPath:storePath]) {
+        NSLog(@"User database not found... copy from bundle.");
+        [[NSFileManager defaultManager] copyItemAtPath:bundleStore toPath:storePath error:nil];
+    } else {
+        NSLog(@"User database found.");
+    }
+}
+
+- (void)copyPublicDatabase
+{
+    NSString *storePath = [XCZUtils getDatabaseFilePath];
+    NSString *bundleStore = [[NSBundle mainBundle] pathForResource:@"xcz" ofType:@"db"];
+    //NSLog(@"%@", storePath);
+    
+    // 若Documents文件夹下不存在数据库文件，则执行拷贝
+    if (![[NSFileManager defaultManager] fileExistsAtPath:storePath]) {
+        NSLog(@"Public database not found... copy from bundle.");
+        [[NSFileManager defaultManager] copyItemAtPath:bundleStore toPath:storePath error:nil];
+    } else {
+        NSLog(@"Public database found.");
+        
+        NSString *latestVersion;
+        NSString *currentVersion;
+        
+        // 获取latestVersion
+        @try {
+            FMDatabase *db = [FMDatabase databaseWithPath:bundleStore];
+            [db open];
+            FMResultSet *s = [db executeQuery:@"SELECT * FROM version"];
+            [s next];
+            latestVersion = [s stringForColumn:@"version"];
+            [db close];
+        }
+        @catch (NSException * e) {
+            NSLog(@"Exception: %@", e);
+        }
+        
+        // 获取currentVersion
+        @try {
+            FMDatabase *db = [FMDatabase databaseWithPath:storePath];
+            [db open];
+            FMResultSet *s = [db executeQuery:@"SELECT * FROM version"];
+            [s next];
+            currentVersion = [s stringForColumn:@"version"];
+            [db close];
+        }
+        @catch (NSException * e) {
+            NSLog(@"Exception: %@", e);
+        }
+        
+        // 若 version 不匹配，则删除原有db，将新db复制过来
+        if(![latestVersion isEqualToString:currentVersion]) {
+            NSLog(@"Version not match...delete old one and copy new one from bundle.");
+            [[NSFileManager defaultManager] removeItemAtPath:storePath error:NULL];
+            [[NSFileManager defaultManager] removeItemAtPath:storePath error:NULL];[[NSFileManager defaultManager] copyItemAtPath:bundleStore toPath:storePath error:nil];
+        } else {
+            NSLog(@"Version match.");
+        }
+    }
+}
+
+@end
