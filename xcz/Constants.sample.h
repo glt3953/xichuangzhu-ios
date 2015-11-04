580f3046d52ad2bb915a96cbb70a2aaf827d107d
hustlzp <hustlzp@qq.com>
2015-11-04 16:26:50 +0800

    Clean codes.

--- xcz/Constants.sample.h
+++ xcz/Constants.sample.h
@@ -1,12 +1,6 @@
 #ifndef Constants_h
 #define Constants_h
 
-#if TARGET_IPHONE_SIMULATOR
-#define RunOnSimulator 1
-#else
-#define RunOnSimulator 0
-#endif
-
 #define AVOSCloudAppID  @""
 #define AVOSCloudAppKey @""
 #define UmengAppKey @""
